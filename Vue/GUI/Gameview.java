02beaa1c6342b31963782e1e0d8930a852af30cf
Thib <thibault.poux@etu.u-paris.fr>
2022-12-26 13:35:39 +0100

    rr

--- Vue/GUI/Gameview.java
+++ Vue/GUI/Gameview.java
@@ -9,9 +9,22 @@ public class Gameview extends JFrame{
 
     public Gameview(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setExtendedState(JFrame.MAXIMIZED_BOTH); 
-        setUndecorated(true);
+        // setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        // setUndecorated(true);
+        setSize(400, 400);
         setVisible(true);
+        this.setLayout(null);
+
+        JPanel panel = new JPanel() ;
+        panel.setVisible(true);
+
+        JLabel texte = new JLabel("dams");
+        texte.setLocation(150, 100);
+        // setLocationRelativeTo(null);
+        // panel.add(texte) ;
+        this. add(texte) ;
+        System.out.println( texte.getLocation() ) ;
+        // texte.setVisible(true);
 
 
         menu = new Menu(this);
