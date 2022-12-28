4458f8de12fb6ffea8b400ae64d23e436cb056e5
Albertu17 <tomasialbert3@gmail.com>
2022-12-28 16:37:08 +0100

    joueurs min et max

--- JeuTuilesGenerique/Modele/Controleur.java
+++ JeuTuilesGenerique/Modele/Controleur.java
@@ -7,7 +7,7 @@ public class Controleur {
     public static void initialisation() {
         Pioche pioche = new Pioche(72);
         Joueurs joueurs = new Joueurs(5);
-        Plateau plateau = new Plateau(4,4);
+        Plateau plateau = new Plateau(5,5);
         Partie partie = new Partie(joueurs, plateau, pioche);
         new GameView(partie);
     }
--- JeuTuilesGenerique/Modele/Joueurs.java
+++ JeuTuilesGenerique/Modele/Joueurs.java
@@ -5,6 +5,8 @@ public class Joueurs {
     public Joueur[] joueurs;
 
     public Joueurs (int nbJoueurs) {
+        nbJoueurs = Math.max(nbJoueurs, 2); // Nombre de joueurs min: 2.
+        nbJoueurs = Math.min(nbJoueurs, 5); // Nombre de joueurs max: 5.
         joueurs = new Joueur[nbJoueurs];
         for (int i = 0; i < nbJoueurs; i++) {
             joueurs[i] = new Joueur(Character.toString(65+i));
