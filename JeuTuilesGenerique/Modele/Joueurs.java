7177581520577475c748c32a4d2f9de57d86ca89
Thib <thibault.poux@etu.u-paris.fr>
2022-12-28 17:21:01 +0100

    Remplacement de le classe joueur
    Notament avec une arrayList au lieu d'un tableau

--- JeuTuilesGenerique/Modele/Joueurs.java
+++ JeuTuilesGenerique/Modele/Joueurs.java
@@ -1,31 +1,74 @@
 package JeuTuilesGenerique.Modele;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Joueurs {
     
-    public Joueur[] joueurs;
+    public List<Joueur> players ; 
+    private static int nombreMaximalDeJoueur = 5 ;
 
     public Joueurs (int nbJoueurs) {
         nbJoueurs = Math.max(nbJoueurs, 2); // Nombre de joueurs min: 2.
         nbJoueurs = Math.min(nbJoueurs, 5); // Nombre de joueurs max: 5.
-        joueurs = new Joueur[nbJoueurs];
+        
         for (int i = 0; i < nbJoueurs; i++) {
-            joueurs[i] = new Joueur(Character.toString(65+i));
+            players.add( new Joueur(Character.toString(65+i), false, true) );
         }
+    }
+
+    public Joueurs(){
+        players = new ArrayList<Joueur>() ;
     }
 
     public int nbJoueurs() {
-        return joueurs.length;
+        return players.size();
     }
 
     public class Joueur {
-
-        public int nbPoints;
-        public String nom;
+        private int score ;
+        private final String nom ;
+        private int NbrPion ;
+        private boolean IA ;  
     
-        public Joueur (String nom) {
-            nbPoints = 0;
-            this.nom = nom;
+    
+        public Joueur(String nom, boolean IA, boolean Pion){
+            this.nom = nom ;
+            score = 0 ;
+            NbrPion = Pion ? 5 :0  ; // à voir le nombre de Pion
+            this.IA = IA ;
         }
+    
+        public int getScore(){return score ;}
+    
+        public void addScore(int ajouter){ score += ajouter ;}
+    
+        public String getName(){ return nom ;}
+    
+    
+    
+        public int getNbrPion() {
+            return NbrPion;
+        }
+    
+        public void setNbrPion(int nbrPion) {
+            NbrPion = nbrPion;
+        }
+    
+        public boolean isIA(){return IA ;}
+        public void setIA(boolean IA){this.IA = IA ;}
+        
     }
 
+    public boolean addPlayer(Joueur playeur){
+        if (players.size() <= nombreMaximalDeJoueur ){
+            players.add(playeur) ;
+            return true ;
+        }
+        return false ;
+    }
+    public Joueur getPlayers(int index){return players.get(index) ;}
+    public List<Joueur> getPlayers(){return players ;}
 }
+
+
