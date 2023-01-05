b79bbac585ba852d9dff60ef59c15a162b621d5b
Thib <thibault.poux@etu.u-paris.fr>
2023-01-05 17:47:36 +0100

    new plateau

--- JeuTuilesGenerique/Modele/Plateau.java
+++ JeuTuilesGenerique/Modele/Plateau.java
@@ -18,6 +18,8 @@ public class Plateau implements Serializable{
     }
 
     private Tuile[][] newplateauFullTuileVide(int hauteur, int largeur){
+        this.hauteur = hauteur ;
+        this.largeur = largeur ;
         plateau = new Tuile[hauteur][largeur];
         // On remplit le plateau de tuiles vides à l'initialisation.
         for (int i = 0; i < (hauteur); i++) {
@@ -30,65 +32,58 @@ public class Plateau implements Serializable{
     
     public void add(Tuile tuile, int x, int y) {
         
-        if (x== 1 || y==1 || x == plateau.length-2 || y == plateau[0].length-2 ){
+        if (x== 1 ){
             Tuile[][] tab = plateau.clone() ;
-            plateau = newplateauFullTuileVide(plateau.length+2, plateau.length+2) ;
+            plateau = newplateauFullTuileVide(plateau.length+1, plateau.length) ;
             
             for (int i = 0; i < tab.length; i++){
                 for (int j = 0; j < tab[0].length ;j++){
-                    plateau[i+1][j+1] = tab[i][j] ; 
+                    plateau[i+1][j] = tab[i][j] ; 
                 }
             }
 
             x = x+1;
+        }
+        else if ( x == plateau.length-2  ){
+            Tuile[][] tab = plateau.clone() ;
+            plateau = newplateauFullTuileVide(plateau.length+1, plateau.length) ;
+            
+            for (int i = 0; i < tab.length; i++){
+                for (int j = 0; j < tab[0].length ;j++){
+                    plateau[i][j] = tab[i][j] ; 
+                }
+            }
+
+        }
+
+        if ( y==1  ){
+            Tuile[][] tab = plateau.clone() ;
+            plateau = newplateauFullTuileVide(plateau.length, plateau.length+1) ;
+            
+            for (int i = 0; i < tab.length; i++){
+                for (int j = 0; j < tab[0].length ;j++){
+                    plateau[i][j+1] = tab[i][j] ; 
+                }
+            }
+
             y = y +1 ;
         }
+        else if ( y == plateau[0].length-2 ){
+            Tuile[][] tab = plateau.clone() ;
+            plateau = newplateauFullTuileVide(plateau.length, plateau.length+1) ;
+            
+            for (int i = 0; i < tab.length; i++){
+                for (int j = 0; j < tab[0].length ;j++){
+                    plateau[i][j] = tab[i][j] ; 
+                }
+            }
+
+        }
+        
         // ajout sur le plateau
         plateau[x][y] = tuile ;
 
-        // creation d'un tableau plus grand si besoin 
-        // if (x == 0){
-        //     Tuile[][] tab = newplateauFullTuileVide(plateau.length+2, plateau.length+2) ;
-        //     for (int i = 0; i < plateau.length; i++){
-        //         for (int j = 0; j < plateau[0].length ;j++){
-        //             tab[i+1][j] = plateau[i][j] ; 
-        //         }
-        //     }
-        //     plateau = tab.clone() ;
-        //     x = 2 ;
-        // }
-        // else if (y <  0){
-        //     Tuile[][] tab = new Tuile[plateau.length][plateau[0].length+1] ; 
-        //     for (int i = 0; i < plateau.length; i++){
-        //         for (int j = 0; j < plateau[0].length ;j++){
-        //             tab[i][j+1] = plateau[i][j] ; 
-        //         }
-        //     }
-        //     plateau = tab.clone() ;
-        //     y = 0;
-        // }
-        // else if (x >= plateau.length){
-        //     Tuile[][] tab = new Tuile[plateau.length+1][plateau[0].length] ; 
-        //     for (int i = 0; i < plateau.length; i++){
-        //         for (int j = 0; j < plateau[0].length ;j++){
-        //             tab[i][j] = plateau[i][j] ; 
-        //         }
-        //     }
-        //     plateau = tab.clone() ;
-            
-        // }
         
-        // else if (y >= plateau[0].length){
-        //     Tuile[][] tab = new Tuile[plateau.length][plateau[0].length+1] ; 
-        //     for (int i = 0; i < plateau.length; i++){
-        //         for (int j = 0; j < plateau[0].length ;j++){
-        //             tab[i][j] = plateau[i][j] ; 
-        //         }
-        //     }
-        //     plateau = tab.clone() ;
-            
-        // }
-
 
     }
 
