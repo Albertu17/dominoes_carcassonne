91f4f1e903b5acf831e3458b0086b0c37d467e96
Albertu17 <tomasialbert3@gmail.com>
2023-01-06 23:13:34 +0100

    suppression Jouable.java

--- JeuTuilesGenerique/Modele/Jouable.java
+++
@@ -1,7 +0,0 @@
-package JeuTuilesGenerique.Modele;
-
-public interface Jouable {
-    
-    // Renvoie false si le coup n'est pas autorisé, et true sinon.
-    public boolean check(Tuile t, int x, int y);
-}
